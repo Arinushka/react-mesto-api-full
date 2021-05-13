b6c859f6c65b49505958a5a3bb5bc6f0cee7dd6d
Arina Mykhova <arinabagira@mail.ru>
2021-05-13 19:15:23 +0300

    feat: развернуть API

--- backend/app.js
+++ backend/app.js
@@ -29,7 +29,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(requestLogger);
 app.use(cors());
-app.options('*', cors());
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
