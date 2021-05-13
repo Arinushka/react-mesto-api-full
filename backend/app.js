f598fefacf0d846dfef1f143301c3a1b59d39557
Arina Mykhova <arinabagira@mail.ru>
2021-05-13 18:56:30 +0300

    feat: развернуть API

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const { celebrate, Joi } = require('celebrate');
 const {
   createUser, login,
@@ -13,7 +14,6 @@ const auth = require('./middlewares/auth');
 const urlValidation = require('./errors/celebrateError');
 const NotFoundError = require('./errors/notFoundError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const cors = require('cors');
 
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
