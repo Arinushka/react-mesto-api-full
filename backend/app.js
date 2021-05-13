dcdf234953ed805901cc4e2b08832ce9af77b69a
Arina Mykhova <arinabagira@mail.ru>
2021-05-13 19:01:14 +0300

    feat: развернуть API

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(cors());
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-const cors = require('cors');
+
 const { celebrate, Joi } = require('celebrate');
 const {
   createUser, login,
@@ -26,7 +28,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use(requestLogger);
-app.use(cors());
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
