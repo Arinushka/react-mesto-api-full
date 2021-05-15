05310704b443e9063866fa5c74ba611c189ab5be
Arina Mykhova <arinabagira@mail.ru>
2021-05-15 20:55:52 +0300

    fix: поправить урну у карточки

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -7,7 +7,6 @@ const InvalidRequestError = require('../errors/invalidRequestError');
 module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
-    .populate(['owner', 'likes'])
     .then((card) => res.send(card))
     .catch((err) => {
       if (err.name === 'CastError') {
