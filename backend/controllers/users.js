572a40e6f35512d4f4971ec30083833b83d11bc6
Arina Mykhova <arinabagira@mail.ru>
2021-05-31 18:56:45 +0300

    feat: сделать удаление токена в куках

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -16,7 +16,7 @@ module.exports.login = (req, res, next) => {
         NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' },
       );
-      res.cookie('jwt', 'Bearer ' + token, { // eslint-disable-line
+      res.cookie('jwt', token, {
         maxAge: 3600000,
         httpOnly: true,
       })
