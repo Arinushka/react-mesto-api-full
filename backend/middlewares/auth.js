3c48dbe4271af8d23ecca1437e9ec4937161da7d
Arina Mykhova <arinabagira@mail.ru>
2021-05-31 17:41:42 +0300

    feat: сделать удаление токена в куках

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,24 +1,20 @@
-const { NODE_ENV, JWT_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorizedError');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
-  const token1 = req.cookies.jwt;
+  const tokenCookie = req.cookies.jwt;
 
-  if (!token1 || !token1.startsWith('Bearer ')) {
+  if (!tokenCookie || !tokenCookie.startsWith('Bearer ')) {
     next(new UnauthorizedError('Необходима авторизация'));
   }
-
-  const cookieToken = token1.replace('Bearer ', '');
+  const token = tokenCookie.replace('Bearer ', '');
   let payload;
-
   try {
-    payload = jwt.verify(cookieToken, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     next(new UnauthorizedError('Необходима авторизация'));
   }
-
   req.user = payload; // записываем пейлоуд в объект запроса
-
-  return next(); // пропускаем запрос дальше
+  next();
 };
